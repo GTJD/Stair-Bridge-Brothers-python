04a62db65465994ffa521c8cf002136d44f56331
Garrett Smith <garrettwhsmith@gmail.com>
2014-05-01 13:30:30 -0500

    update resources file

--- src/game/resources.py
+++ src/game/resources.py
@@ -3,7 +3,6 @@ import pyglet
 pyglet.resource.path = ['../res']
 pyglet.resource.reindex()
 
-red = pyglet.resource.image("red.png")
 box = pyglet.resource.image("box.png")
 checkpoint = pyglet.resource.image("checkpoint.png")
 background = pyglet.resource.image("background.png")
